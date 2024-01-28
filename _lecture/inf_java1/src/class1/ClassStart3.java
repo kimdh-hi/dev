c46397826f0167a48d8d4f4ea5148ce317ad3b7e
daehyunKim <zbeld123@gmail.com>
2024-01-28 16:21:39 +0900

    lecture(java1): object array

--- _lecture/inf_java1/src/class1/ClassStart3.java
+++ _lecture/inf_java1/src/class1/ClassStart3.java
@@ -14,5 +14,15 @@ public class ClassStart3 {
 
         System.out.println(student1);
         System.out.println(student2);
+
+        Student[] students = new Student[2];
+        System.out.println(students[0]); // 초기화하지 않은 경우 nul
+        students[0] = student1;
+        students[1] = student2;
+
+        System.out.println("student1 name: " + student1.name);
+        students[0].name = "updatedKimName"; // 같은 참조값을 가지므로 student1 도 갱신
+        System.out.println("student1 name: " + student1.name);
+
     }
 }
