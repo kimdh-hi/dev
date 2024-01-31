bac10d77dcf5e4a23b858dda6a3ea7a819969470
daehyunKim <zbeld123@gmail.com>
2024-01-31 10:29:45 +0900

    lecture(java1): constructor overloading

--- _lecture/inf_java1/src/constructor/ConstructorOverloading.java
+++ _lecture/inf_java1/src/constructor/ConstructorOverloading.java
@@ -10,7 +10,8 @@ public class ConstructorOverloading {
     }
 
     ConstructorOverloading(String value1) {
-        this(value1, "default");
+//        System.out.println("..."); // 생성자 첫줄에 다른 코드가 있는 경우 컴파일 에러
+        this(value1, "default"); // 생성자 내 첫줄에만 사용 가능
 //        this.value1 = value1;
 //        this.value2 = "default";
     }
